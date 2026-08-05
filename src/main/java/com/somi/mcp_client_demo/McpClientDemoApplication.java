9bb5462dfef1953de9c11d2f9e2c84e9907f5be3
somixyz <milosh.dr93@yahoo.com>
2026-08-06 01:06:32 +0200

    init commit

---
+++ src/main/java/com/somi/mcp_client_demo/McpClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.somi.mcp_client_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somi/mcp_client_demo/McpClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somi.mcp_client_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
